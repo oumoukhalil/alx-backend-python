0f7964732d3a04dfa807dfd0e2739847aa2712fd
oumoukhalil <fatsarr628@gmail.com>
2024-06-06 14:29:59 +0000

    2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import math
+"""module math"""
+
+def floor(n:float)->int:
+    """function floor
+
+    params: n (float)
+
+    return: int
+
+    """
+    return (math.floor(n))
